5b6eec2e4a26feb1d376aee1282a6346de58e3df
mjcarbonell <maximcarbonell@gmail.com>
2024-09-12 12:07:26 -0700

    saving world

--- start.sh
+++ start.sh
@@ -107,6 +107,28 @@ add_operator() {
     fi
 }
 add_operator
+set_world_folder() {
+    WORLD_PATH="/WorldBackup/your_world_folder"
+
+    if [ -d "$WORLD_PATH" ]; then
+        if [ ! -f server.properties ]; then
+            echo "Creating server.properties file..."
+            echo "level-name=$WORLD_PATH" > server.properties
+        else
+            echo "Setting level-name in server.properties..."
+            if grep -q "level-name=" server.properties; then
+                sed -i "s|level-name=.*|level-name=$WORLD_PATH|" server.properties
+            else
+                echo "level-name=$WORLD_PATH" >> server.properties
+            fi
+        fi
+        echo "World folder found, configured to load from $WORLD_PATH"
+    else
+        echo "World folder does not exist. Skipping world loading configuration."
+    fi
+}
+set_world_folder
+
 runJavaCommand() {
   "$JAVA" ${1}
 }
